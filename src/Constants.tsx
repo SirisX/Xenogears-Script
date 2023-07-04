7dfaf1b410f9f4906638bcf499e6bdd9ae52adf9
SirisX <116883013+SirisX@users.noreply.github.com>
2023-07-04 02:30:16 -0700

    Update Constants.tsx

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -3,6 +3,8 @@ import text0000 from './text/0000_CONTENTS.txt'
 import text000 from './text/000_Opening_Movie.txt'
 import text00 from './text/00_Prologue.txt'
 import text01 from './text/01_Lahan_Village.txt'
+import text02 from './text/02_Mountain_Path.txt'
+import text03 from './text/03_House_On_a_Hill.txt'
 
 export const DefaultChapters: Chapter[] = [
     {
