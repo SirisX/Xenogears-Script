211516f87f8734cf3835600dcd881955b96d821e
SirisX <116883013+SirisX@users.noreply.github.com>
2023-08-10 01:05:02 -0700

    Update Constants.tsx
    +Chapter 44

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -49,6 +49,7 @@ import chap40Text from "./text/chapters/40_Babel_Tower.txt";
 import chap41Text from "./text/chapters/41_Sky_City_Shevat.txt";
 import chap42Text from "./text/chapters/42_Intruder_Alert.txt";
 import chap43Text from "./text/chapters/43_Raid_of_Shevat.txt";
+import chap44Text from "./text/chapters/44_Gate1_Margie.txt";
 
 import jpHeaderText from "./text/intro/0_JP_HEADER.txt";
 import jpContentsText from "./text/intro/01_JP_CONTENTS.txt";
@@ -430,6 +431,13 @@ export const DefaultChapters: Chapter[] = [
     name: "Raid of Shevat!",
     number: 43,
     text: chap43Text,
+    japaneseText: SWAP_ME_OUT,
+    // japaneseName: SWAP_ME_OUT,
+  },
+  {
+    name: "Gate1 -Margie",
+    number: 44,
+    text: chap44Text,
     japaneseText: SWAP_ME_OUT,
     // japaneseName: SWAP_ME_OUT,
   }
